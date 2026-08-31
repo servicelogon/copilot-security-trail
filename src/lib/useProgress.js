3757e493de8d62a3705aeabde803bebd0ea6570b
Nate <natehess@protonmail.com>
2026-08-31 11:00:09 -0400

    Replace with static trail app

--- src/lib/useProgress.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ProgressContext } from "./progress-context";
-
-export function useProgress() {
-  const ctx = useContext(ProgressContext);
-  if (!ctx) throw new Error("useProgress must be used within ProgressProvider");
-  return ctx;
-}
